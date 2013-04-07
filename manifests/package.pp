5ab6c69763405f7dd72295eac5021b9f3998be68
Lucas Bickel <hairmare@purplehaze.ch>
2013-04-07 13:38:11 +0200

    configure portage before installing server package

--- manifests/package.pp
+++ manifests/package.pp
@@ -4,6 +4,13 @@ class freeradius::package(
 ) {
 
   if $server == true {
+    if $::operatingsystem == Gentoo {
+      file{ '/etc/portage/package.use/10_freeradius':
+        content => 'net-dialup/freeradius pam readline ssl ldap python ruby'
+      } -> file { '/etc/portage/package.use/10_freeradius__openldap':
+        content => 'net-nds/openldap minimal'
+      } -> Package[$freeradius::params::radius['packages']]
+    }
     package { $freeradius::params::radius['packages']:
       ensure => present,
     }
