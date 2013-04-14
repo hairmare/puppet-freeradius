ff65320a0bccc753a152b3ed82328cd31ef270cd
Lucas Bickel <hairmare@purplehaze.ch>
2013-04-14 22:01:49 +0200

    port proxy stuff to service nodes

--- manifests/server.pp
+++ manifests/server.pp
@@ -17,6 +17,7 @@ define freeradius::server (
   $authorize_wimax                = $freeradius::params::vhost['authorize']['wimax'],
   $authorize_ipass                = $freeradius::params::vhost['authorize']['ipass'],
   $authorize_suffix               = $freeradius::params::vhost['authorize']['suffix'],
+  $authorize_proxy                = $freeradius::params::vhost['authorize']['proxy'],
   $authorize_ntdomain             = $freeradius::params::vhost['authorize']['ntdomain'],
   $authorize_eap                  = $freeradius::params::vhost['authorize']['eap'],
   $authorize_unix                 = $freeradius::params::vhost['authorize']['unix'],
